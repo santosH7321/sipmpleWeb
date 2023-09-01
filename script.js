a9aa453fe286839239eae07b6ed3faffb0333db0
Santosh <santoshkumar23kky@gmail.com>
2023-09-01 23:14:11 +0530

    simpleWeb

---
+++ script.js
@@ -0,0 +1,11 @@
+var typeDate = new Typed (".role",{
+    Strings: [
+        "Full stack Devloper",
+        "Web Devloper",
+        "Coder"
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDeley: 1000,
+});
